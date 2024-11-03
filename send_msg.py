35353ede1fc115727e79ea4b859abdf28c46a6a0
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-11-02 23:53:36 -0400

    sync

--- send_msg.py
+++ send_msg.py
@@ -12,7 +12,7 @@ auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 alfred = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-def get_conversation_history(account_sid: str, auth_token: str, phone_number: str) -> List[Dict]:
+def get_conversation_history(phone_number: str) -> List[Dict]:
     
     messages = client.messages.list(
         to=phone_number,
@@ -50,21 +50,20 @@ def print_conversation(conversation: List[Dict]) -> None:
         print(f"    Status: {msg['status']}")
         print("-" * 50)
 
-def prompt_gpt (prompt: str) -> str:
+def prompt_gpt (context: str) -> str:
     output = alfred.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are an event organizer for a group of friends. You are helping them to find a place and time that works for everyone. You will suggests "},
-            {"role": "user", "content": "Hello"}
+            {"role": "system", "content": "You are Alfred, an event organizer for a group of friends. You are helping them to find a place and time that works for everyone. You will suggest time to meet for the group"},
+            {"role": "context", "content": context},
         ]
     )
     print(output.choices[0].message.content)
     return output.choices[0].message.content
 
 if __name__ == '__main__':
-    prompt_gpt("Hello")
     client.messages.create(
-        to="+16143448385",
+        to="+16268785556",
         from_="+18556049506",
-        body=prompt_gpt("Hello")
+        body=prompt_gpt(get_conversation_history("+16268785556")[-1]['body'])
     )
